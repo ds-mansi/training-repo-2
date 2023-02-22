bb7202022f3cfee022b7ecf24a361a86147f7811
ds-mansi <mansi.gurnani@dotsquares.com>
2023-02-22 18:06:55 +0530

    phone css

--- src/components/locatorPage/LocationCard.tsx
+++ src/components/locatorPage/LocationCard.tsx
@@ -156,7 +156,7 @@ function opentime(e: any) {
           </div>
           <div className="flex ml-7 mt-2">
           <img src={phone} style={{height:"30px"}}/>
-          <div style={{fontSize:"20px"}}>{mainPhone}</div>
+          <div style={{fontSize:"18px"}}>{mainPhone}</div>
           </div>
           
           </div>
